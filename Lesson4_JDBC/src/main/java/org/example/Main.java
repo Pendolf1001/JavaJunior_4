f789e2f85c5be54406bab7c1adbdf4812d47c67c
Evgeniy K <kovalev8915512@gmail.com>
2024-12-19 14:46:57 +0300

    Podkluchil mySQL iz Linux

--- Lesson4_JDBC/src/main/java/org/example/Main.java
+++ Lesson4_JDBC/src/main/java/org/example/Main.java
@@ -1,7 +1,30 @@
 package org.example;
 
+import com.sun.jdi.connect.spi.Connection;
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws java.sql.SQLException {
+
+
+
         System.out.println("Hello world!");
+
+        java.sql.Connection conn= DriverManager.getConnection("jdbc:mysql://192.168.1.138:3306","lesson4","1" );
+
+        createDatabase(conn);
     }
+
+    public static void createDatabase(java.sql.Connection connection) throws SQLException{
+        String createDataBaseSQL = "CREATE DATABASE IF NOT EXISTS testDB;";
+        try (PreparedStatement statement=connection.prepareStatement(createDataBaseSQL)){
+            statement.execute();
+
+        }
+    }
+
+
 }
